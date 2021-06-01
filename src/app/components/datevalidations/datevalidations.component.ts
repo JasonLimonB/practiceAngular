15447d62abb97c2374c21c1c90b61686dd3bcac9
Jason <jason.limon@outlook.com>
2021-05-31 20:53:12 -0500

    Adding method

--- src/app/components/datevalidations/datevalidations.component.ts
+++ src/app/components/datevalidations/datevalidations.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { DateVService } from '../../services/validations/date-v.service';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-datevalidations',
   templateUrl: './datevalidations.component.html',
-  styleUrls: ['./datevalidations.component.scss']
+  styleUrls: ['./datevalidations.component.scss'],
+  providers: [ DateVService ]
 })
 export class DatevalidationsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private _service: DateVService ) { }
+
+  
+  PlanningWEDate: Date = this._service.setWeekEndingDate(moment(new Date()).utc().toDate());
+  PublishedWEDate: Date = this._service.setWeekEndingDate(moment(new Date()).add(-7, 'days').utc().toDate());
 
   ngOnInit(): void {
+    console.log(`PlanningWEDate -> ${this.PlanningWEDate}`);
+    console.log(`PublishedWEDate -> ${this.PublishedWEDate}`);
+    
+    
   }
 
 }
--- src/app/services/validations/date-v.service.ts
+++ src/app/services/validations/date-v.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
 
 @Injectable()
 export class DateVService {
-
   constructor() { }
 
-  validateDate( date: Date ){
-
+  public setWeekEndingDate( date: Date ): any {
+    const currentDay = date.getDay();
+    switch (currentDay) {
+      case 0:
+        return moment(date).add(13, 'days').toDate();
+      case 1:
+        return moment(date).add(12, 'days').toDate();
+      case 2:
+        return moment(date).add(11, 'days').toDate();
+      case 3:
+        return moment(date).add(10, 'days').toDate();
+      case 4:
+        return moment(date).add(9, 'days').toDate();
+      case 5:
+        return moment(date).add(15, 'days').toDate();
+      case 6:
+        return moment(date).add(14, 'days').toDate();
+    }
   }
 
 }
